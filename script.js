f2fe5b9a244cbfe864e228bbfbbdbaf387a24c12
thithirss <thiagovicente2@gmail.com>
2023-10-20 12:43:54 -0300

    third commit

--- script.js
+++ script.js
@@ -3,19 +3,32 @@ document.addEventListener("DOMContentLoaded", function() {
     let slideIndex = 0;
     const slides = slider.querySelectorAll("img");
     const slideCount = slides.length;
-
+  
     function nextSlide() {
-        if (slideIndex === slideCount - 1) {
-            slideIndex = 0;
-        } else {
-            slideIndex++;
-        }
-        updateSlider();
+      if (slideIndex === slideCount - 1) {
+        slideIndex = 0;
+      } else {
+        slideIndex++;
+      }
+      updateSlider();
     }
-
+  
     function updateSlider() {
-        slider.style.transform = `translateX(-${slideIndex * 100}%)`;
+      slider.style.transform = `translateX(-${slideIndex * 100}%)`;
     }
-
+  
     setInterval(nextSlide, 3000);
-});
+  
+    const cards = document.querySelectorAll('.card');
+  
+    cards.forEach(card => {
+      card.addEventListener('click', () => {
+        card.querySelector('.card-inner').style.transform = 'rotateY(180deg)';
+      });
+  
+      card.addEventListener('mouseleave', () => {
+        card.querySelector('.card-inner').style.transform = 'rotateY(0deg)';
+      });
+    });
+  });
+  
