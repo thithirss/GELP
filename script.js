be02e915d149791c47d0c7d7064c5e085ab981a5
Thiago Vicente <thiagovicente2@gmail.com>
2023-10-25 15:06:49 -0300

    new realease

--- script.js
+++ script.js
@@ -1,34 +1,33 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const slider = document.querySelector(".slider");
-    let slideIndex = 0;
-    const slides = slider.querySelectorAll("img");
-    const slideCount = slides.length;
-  
-    function nextSlide() {
-      if (slideIndex === slideCount - 1) {
-        slideIndex = 0;
-      } else {
-        slideIndex++;
-      }
-      updateSlider();
+  const slider = document.querySelector(".slider");
+  let slideIndex = 0;
+  const slides = slider.querySelectorAll("img");
+  const slideCount = slides.length;
+
+  function nextSlide() {
+    if (slideIndex === slideCount - 1) {
+      slideIndex = 0;
+    } else {
+      slideIndex++;
     }
-  
-    function updateSlider() {
-      slider.style.transform = `translateX(-${slideIndex * 100}%)`;
-    }
-  
-    setInterval(nextSlide, 3000);
-  
-    const cards = document.querySelectorAll('.card');
-  
-    cards.forEach(card => {
-      card.addEventListener('click', () => {
-        card.querySelector('.card-inner').style.transform = 'rotateY(180deg)';
-      });
-  
-      card.addEventListener('mouseleave', () => {
-        card.querySelector('.card-inner').style.transform = 'rotateY(0deg)';
-      });
+    updateSlider();
+  }
+
+  function updateSlider() {
+    slider.style.transform = `translateX(-${slideIndex * 100}%)`;
+  }
+
+  setInterval(nextSlide, 3000);
+
+  const cards = document.querySelectorAll('.card');
+
+  cards.forEach(card => {
+    card.addEventListener('click', () => {
+      card.querySelector('.card-inner').style.transform = 'rotateY(180deg)';
+    });
+
+    card.addEventListener('mouseleave', () => {
+      card.querySelector('.card-inner').style.transform = 'rotateY(0deg)';
     });
   });
-  
+});
