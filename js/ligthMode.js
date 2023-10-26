28304431d4b8c96eb588cf376971f032cfe7fba5
Pedro Lucas <pedro_caslu@outlook.com>
2023-10-26 16:40:09 -0300

    Final Commit

--- js/ligthMode.js
+++ js/ligthMode.js
@@ -1,17 +1,17 @@
-const mode = document.getElementById('mode-icon');
+const modo = document.getElementById('mode-icon');
 
-mode.addEventListener('click', () => {
+modo.addEventListener('click', () => {
     const body = document.getElementById('body');
-    if(mode.classList.contains('fa-sun')){
-        mode.classList.remove('fa-sun');
-        mode.classList.add('fa-moon');
+    if(modo.classList.contains('fa-sun')){
+        modo.classList.remove('fa-sun');
+        modo.classList.add('fa-moon');
 
         body.classList.add('light')
         return;
     }
 
-    mode.classList.remove('fa-moon');
-    mode.classList.add('fa-sun');
+    modo.classList.remove('fa-moon');
+    modo.classList.add('fa-sun');
     body.classList.remove('light')
 });
 
